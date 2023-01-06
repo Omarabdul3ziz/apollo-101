95641e50fc03b7392077377059fff939c4da1dbb
omarabdul3ziz <omarabdul3ziz@gmail.com>
2023-01-06 15:31:57 +0200

    foo

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ export const server = new ApolloServer({
   plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 server
   .listen({
